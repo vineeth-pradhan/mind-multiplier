993ddce4d0bf06c272c5430d9faa07d1c0bbbf2a
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-04-24 05:46:00 +0530

    Reactifying one.js

--- pages/tables/play/one.js
+++ pages/tables/play/one.js
@@ -1,43 +1,51 @@
-import Link from 'next/link'
 import Head from 'next/head'
 import Layout from '../../../components/layout'
-import { useEffect } from 'react'
+import React from 'react';
 
-export default function NewTable(){
+class NewTable extends React.Component{
+  constructor(){
+    super()
+  }
 
-  useEffect(() => {
-    document.onload = function() {
-      var element = document.getElementById("get-new-table-input");
-      element.querySelector("input[name='which-table']").focus();
-    }()
-  })
+  componentDidMount(){
+    this.focusOnInput();
+  }
 
-  var handleSubmit = function(e){
+  focusOnInput(){
+    var form = document.getElementById("get-new-table-input");
+    form.querySelector("input[name='which-table']").focus();
+  }
+
+  handleSubmit(e){
     e.preventDefault();
   }
 
-  return(
-    <Layout>
-      <Head>
-        <title>
-          Mind Multiplier
-        </title>
-      </Head>
-      <div className="h-56 grid grid-cols-3 gap-4 place-items-center ...">
-        <div></div>
-        <form id="get-new-table-input" onSubmit={handleSubmit}>
-          <div>
-            <label> What table would you like to see? </label>
-          </div>
-          <div className="mt-3">
-            <input type="text" name="which-table" className="border border-black rounded-full text-xl indent-3 focus-visible:outline-none" />
-            <button type="submit" className="ml-3 px-2 py-2 font-semibold rounded-full border border-sky-700 text-sm bg-sky-500 hover:bg-sky-700">
-              Submit
-            </button>
-          </div>
-        </form>
-        <div></div>
-      </div>
-    </Layout>
-  );
+  render(){
+    return(
+      <Layout>
+        <Head>
+          <title>
+            Mind Multiplier
+          </title>
+        </Head>
+        <div className="h-56 grid grid-cols-3 gap-4 place-items-center ...">
+          <div></div>
+          <form id="get-new-table-input" onSubmit={this.handleSubmit}>
+            <div>
+              <label> What table would you like to see? </label>
+            </div>
+            <div className="mt-3">
+              <input type="text" name="which-table" className="border border-black rounded-full text-xl indent-3 focus-visible:outline-none" />
+              <button type="submit" className="ml-3 px-2 py-2 font-semibold rounded-full border border-sky-700 text-sm bg-sky-500 hover:bg-sky-700">
+                Submit
+              </button>
+            </div>
+          </form>
+          <div></div>
+        </div>
+      </Layout>
+    );
+  }
 }
+
+export default NewTable;
