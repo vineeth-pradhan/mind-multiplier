6cdb1c98e792f95663d9bd1ecf349e038f9fe82d
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-05-13 22:08:23 +0530

    Adding styling to score display

--- components/score.js
+++ components/score.js
@@ -5,8 +5,12 @@ function Score({score}){
   return(
     <div className="fixed left-[1390px] max-w-[20%] min-w-[20%] overflow-y-auto max-h-[80%]">
       <h1 className="sticky top-0 text-4xl px-[35%] flex items-center selection:bg-fuchsia-300 bg-slate-50/90 dark:bg-slate-700/90 backdrop-blur-sm"> Score </h1>
-      <div>
-        {score}
+      <div className="stats shadow">
+        <div className="stat">
+          <div className="stat-title">Current Score</div>
+          <div className="stat-value text-primary">{score}</div>
+          <div className="stat-desc">21% more than last month</div>
+        </div>
       </div>
     </div>
   )
